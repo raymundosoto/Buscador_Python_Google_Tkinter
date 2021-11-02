3a54a1bb986a8a541f1f2012d510c88dc43fe559
M. En C. Raymundo Soto <72757419+raymundosoto@users.noreply.github.com>
2021-11-02 16:07:06 -0600

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Programa que hace búsqueda de dominios usando google search basada en una o varias palabras.
+el programa regresa las primeras 100 páginas del buscador de google en inglés.
+
+
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 15:40:44 2021
+
+@author: Precision
+"""
+
+
+from distutils.core import setup
+setup(
+  name = 'Buscador',
+  packages = ['Buscador'],
+  version = '0.1',
+  description = 'Buscador de dominios con Python, Google y tkinter',
+  author = 'Raymundo Soto',
+  author_email = 'raymundo_quimico@hotmail.com',
+  url = '',
+  keywords = ['Python', 'Google', 'Tkinter'],
+  license = 'GPLv3',
+  classifiers = [],
+)
